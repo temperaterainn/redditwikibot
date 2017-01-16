6fd2bf5550f590475ef80963bd363c40812d81b3
Arian Nasr <arian@ariannsr.xyz>
2017-01-16 18:43:37 -0500

    Made Script Simpler
    Made code easier to read and added important features.

--- wikibot.py
+++ wikibot.py
@@ -8,16 +8,23 @@ reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit('all')
 comments = subreddit.stream.comments()
 
-print("wikibot started successfully")
+print("Wikipedia! started successfully")
 
 for comment in comments:
     text = comment.body
     inputtext = comment.body
     author = comment.author
-
-    if 'wikibot!' in text.lower():
-        inputtext = inputtext.replace('wikibot!', '')
-        outputtext = wikipedia.summary(inputtext)
-        message = "**wikibot says: **" + outputtext
-        comment.reply(message)
-        print("Replied to a request about: ", inputtext)
+    
+    # Looping the script
+    while True:
+        # If the text 'Wikipedia!' is found
+        if 'Wikipedia!' in text.lower():
+            # Remove 'Wikipedia!' from the search term
+            inputtext = inputtext.replace('Wikipedia!', '')
+            inputtext = inputtext.replace('wikipedia!', '')
+            # Run the search term through Wikipedia
+            outputtext = wikipedia.summary(inputtext)
+            # Add the summary with some fun text
+            message = "Wikipedia says: " + outputtext
+            # Reply to the search request with the summary
+            comment.reply(message)
